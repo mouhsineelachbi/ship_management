ce4187d204cc198d84d55df3ec34010a01f5ded6
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-06 17:14:44 +0000

    create ship model

---
+++ ship_management/Models/Ship.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ship_management.Models
+{
+    [Table("ships")]
+    public class Ship
+    {
+        [Required, Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public double length { get; set; }
+
+        [Required]
+        public double width { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]{4}-[0-9]{4}-[a-zA-Z]{1}[0-9]{1}$", ErrorMessage = "The field {0} is not a valid ship code")]
+        public string code { get; set; }
+    }
+}
