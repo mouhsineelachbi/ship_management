533407102e67825266df08fd0cd5a9ec3c2444ca
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-06 17:13:43 +0000

    add db context

---
+++ ship_management/DB/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ship_management.Models;
+
+namespace ship_management.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Ship> ships { get; set; }
+    }
+}
