7d435e17f8b6c001ddcd8285f21bb7dbd004d82a
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-06 17:12:25 +0000

    init project

---
+++ ship_management/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using ship_management.DB;
+using ship_management.Interfaces;
+using ship_management.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register Db context
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data source=ship.db")); //.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+
+// Register Ship Repository
+builder.Services.AddScoped<IShipRepository, ShipRepository>();
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
