9af4ea888c484fc5a354b733c97b0b012c9a91ac
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-06 17:14:31 +0000

    implement generic repository

---
+++ ship_management/Repositories/RepositoryBase.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ship_management.DB;
+using ship_management.Interfaces;
+using System.Linq.Expressions;
+
+namespace ship_management.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected AppDbContext ctx { set; get; }
+        public RepositoryBase(AppDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        public void Create(T entity)
+        {
+            this.ctx.Set<T>().Add(entity);
+            this.ctx.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            this.ctx.Set<T>().Remove(entity);
+            this.ctx.SaveChanges();
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return this.ctx.Set<T>().AsNoTracking();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this.ctx.Set<T>().Where(expression).AsNoTracking();
+        }
+
+        public void Update(T entity)
+        {
+            this.ctx.Set<T>().Update(entity);
+            this.ctx.SaveChanges();
+        }
+    }
+}
