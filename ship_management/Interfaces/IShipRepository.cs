6854938b69798d5c0103b0327ea1d5cdf201b117
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-06 17:15:12 +0000

    create ship repository abstraction

---
+++ ship_management/Interfaces/IShipRepository.cs
@@ -0,0 +1,14 @@
+﻿using ship_management.Models;
+using ship_management.Paging;
+
+namespace ship_management.Interfaces
+{
+    public interface IShipRepository : IRepositoryBase<Ship>
+    {
+        Task<PagedList<Ship>> GetShips(PagingParameters pagingParameters);
+        Ship GetShip(int id);
+        void CreateShip(Ship ship);
+        void UpdateShip(Ship ship);
+        void DeleteShip(Ship ship);
+    }
+}
