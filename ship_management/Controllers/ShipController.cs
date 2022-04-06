40e015960375a198ce976197439da5b334b6c602
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-06 17:16:32 +0000

    create controller for ship

---
+++ ship_management/Controllers/ShipController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ship_management.Interfaces;
+using ship_management.Models;
+using ship_management.Paging;
+
+namespace ship_management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipController : ControllerBase
+    {
+        private IShipRepository _shipRepository;
+        public ShipController(IShipRepository repo)
+        {
+            this._shipRepository = repo;
+        }
+
+        // Get All Ships
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ship>>> GetShips([FromQuery] PagingParameters pagingParameters)
+        {
+            return await _shipRepository.GetShips(pagingParameters);
+        }
+
+
+        // Get one ship by id
+        [HttpGet("{id}")]
+        public ActionResult<Ship> GetShipById(int id)
+        {
+            var ship = _shipRepository.GetShip(id);
+            if (ship == null)
+            {
+                return NotFound();
+            }
+            return Ok(ship);
+        }
+
+        // Add one Ship
+        [HttpPost]
+        public ActionResult<Ship> CreateShip([FromBody] Ship ship)
+        {
+            if (ship == null)
+            {
+                return BadRequest("Ship object is null");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Ship object");
+            }
+            _shipRepository.CreateShip(ship);
+            return Ok(CreatedAtAction("id", new { id = ship.id }, ship));
+        }
+
+        // Update one Ship by id
+        [HttpPut]
+        public ActionResult<Ship> UpdateShip(int id, [FromBody] Ship ship)
+        {
+            if (ship == null)
+            {
+                return BadRequest("Ship object is null");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Ship object");
+            }
+            var dbShip = _shipRepository.GetShip(id);
+            if (dbShip == null)
+            {
+                return NotFound();
+            }
+            _shipRepository.UpdateShip(ship);
+            return NoContent();
+        }
+        
+
+        // Delete Ship by id
+        [HttpDelete("{id}")]
+        public ActionResult<Ship> DeleteShip(int id)
+        {
+            var dbShip = _shipRepository.GetShip(id);
+            if (dbShip == null)
+            {
+                return NotFound();
+            }
+            _shipRepository.DeleteShip(dbShip);
+            return NoContent();
+        }
+    }
+}
