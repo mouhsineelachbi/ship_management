d985c0796bce41e1d6666b6e0a3e10eae990c8ca
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-07 20:17:53 +0000

    declare bootstrap and primereact and change file structure

--- client_app/src/index.js
+++ client_app/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                            //icons
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
