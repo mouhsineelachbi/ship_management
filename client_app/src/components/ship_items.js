97b70bbf82d3a06dbdecdd863daa848e3fcfcfc1
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-09 01:15:25 +0000

    clean ship items compoennt js file

--- client_app/src/components/ship_items.js
+++ client_app/src/components/ship_items.js
@@ -191,15 +191,6 @@ const ShipItems = () => {
     return index;
   };
 
-  // const createId = () => {
-  //     let id = '';
-  //     let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  //     for (let i = 0; i < 5; i++) {
-  //         id += chars.charAt(Math.floor(Math.random() * chars.length));
-  //     }
-  //     return id;
-  // }
-
   const confirmDeleteSelected = () => {
     setDeleteShipsDialog(true);
   };
