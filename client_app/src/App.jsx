6d226319d0315f182205bb5873f4db408fc1f8c8
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-15 23:44:45 +0000

    connect main component to state manager

--- client_app/src/App.jsx
+++ client_app/src/App.jsx
@@ -1,12 +1,27 @@
-import './App.css';
-import ShipItems from './components/ship_items/ship.items';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import ErrorPage from "./components/error.page/error.page";
+import Loading from "./components/loading.page/loading.screen";
+import ShipItems from "./components/ship.items/ship.items";
+import { getAllShips } from "./feature/ship/ship.slice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllShips());
+  }, []);
+
+  const { isLoading, errorMessage, ships } = useSelector(
+    (state) => state.ships
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <ShipItems />
-      </header>
+      {isLoading && <Loading />}
+      {ships && <ShipItems />}
+      { errorMessage && <ErrorPage />}
     </div>
   );
 }
