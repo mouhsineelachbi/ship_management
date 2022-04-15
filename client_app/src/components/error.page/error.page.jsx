ea98f4f23eb122c9898793536de37e57403b2416
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-15 23:34:42 +0000

    add error page with spinning icon

---
+++ client_app/src/components/error.page/error.page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BiError } from "react-icons/bi";
+import { useSelector } from "react-redux";
+
+const ErrorPage = () => {
+  const { errorMessage } = useSelector((state) => state.ships);
+  return (
+    <div className="row h-100 w-100 d-flex align-content-center ">
+      <div className="col-12 col-sm-12 col-md-12 d-flex justify-content-center text-danger">
+        <BiError size={200} />
+      </div>
+      <div className="col-12 col-sm-12 col-md-12 d-flex justify-content-center"><h1 className="text-danger">{ errorMessage }</h1></div>
+      <div className="col-12 col-sm-12 col-md-12 d-flex justify-content-center text-danger"><h5>Please Try Again</h5></div>
+      
+    </div>
+  );
+};
+
+export default ErrorPage;
