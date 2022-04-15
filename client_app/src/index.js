bf0d6cc7fe3acd6794e3ed169c129749f25892c1
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-15 23:44:01 +0000

    subscribe to store

--- client_app/src/index.js
+++ client_app/src/index.js
@@ -6,22 +6,15 @@ import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css";
 import { createRoot } from "react-dom/client";
-import { createStore } from "redux";
-import { allReducers } from "./reducers";
 import { Provider } from "react-redux";
-
-const store = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
+import store from "./feature/ship/store";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+
+
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
 );
