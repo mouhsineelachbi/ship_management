be87bd9dd47fb2c9437cdbe4f0ba000d449281b9
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-20 18:26:23 +0000

    create login user and user dto's

---
+++ ship_management/DTO/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ship_management.DTO
+{
+    public class LoginDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; }
+    }
+}
---
+++ ship_management/DTO/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ship_management.DTO
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
