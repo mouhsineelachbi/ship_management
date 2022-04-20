c081a416cbc64e6c0095af609d364ade5d846d9f
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-20 18:24:58 +0000

    add user model

---
+++ ship_management/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace ship_management.Models
+{
+    public class User
+    {
+        [Required, Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [JsonIgnore]
+        public string Password { get; set; }
+
+    }
+}
