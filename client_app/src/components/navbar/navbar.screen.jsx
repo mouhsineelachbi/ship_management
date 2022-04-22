7b864661785cb64195cf02694109b97a585bf390
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-22 15:04:11 +0000

    show login and register for non logged in user and logout for logged in users

--- client_app/src/components/navbar/navbar.screen.jsx
+++ client_app/src/components/navbar/navbar.screen.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, Outlet } from "react-router-dom";
+import { logoutUser } from "../../feature/user/user.slice";
+import './nav.screen.css';
 
 export default function Navbar() {
+  const { userLoading, user, error } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(logoutUser());
+  };
+
   return (
     <div>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link to="/" className="navbar-brand">
             Home
-          </a>
+          </Link>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarCollapse"
@@ -17,25 +28,43 @@ export default function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="collapse navbar-collapse" id="navbarCollapse">
-            <ul className="navbar-nav me-auto mb-md-8 d-flex justify-content-end w-100">
-              <li className="nav-item active mx-2">
-                <a className="nav-link" href="#">
-                  Login
-                </a>
-              </li>
-              <li className="nav-item active mx-2">
-                <a className="nav-link" href="">
-                  Register
-                </a>
-              </li>
-            </ul>
-          </div>
+          {!user ? (
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <ul className="navbar-nav me-auto mb-md-8 d-flex justify-content-end w-100">
+                <li className="nav-item mx-2">
+                  <Link to="/login" className="nav-link">
+                    <h5>Login</h5>
+                  </Link>
+                </li>
+                <li className="nav-item mx-2">
+                  <Link to="/register" className="nav-link">
+                    <h5>Register</h5>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <ul className="navbar-nav me-auto mb-md-8 d-flex justify-content-end w-100">
+                <li className="nav-item active mx-2">
+                  <span className="nav-link">
+                    Welcome {user.username}
+                  </span>
+                </li>
+                <li className="nav-item active mx-2">
+                  <Link to="/login" className="nav-link" onClick={logout}>
+                    <h5>Logout</h5>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
+      <Outlet />
     </div>
   );
 }
