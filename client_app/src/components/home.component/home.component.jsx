5ce5632e274f5c61e3155324c5b435c1980c3451
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-22 15:03:02 +0000

    create home component and set Its content to Shipitems

---
+++ client_app/src/components/home.component/home.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import {getUser} from "../../feature/user/user.slice";
+import ShipItems from '../ship.items/ship.items';
+
+export default function Home() {
+
+  const dispatch = useDispatch();
+  const {userLoading, user, error} = useSelector(state => state.user);
+
+    useEffect(()=>{
+      dispatch(getUser())
+      .then(
+        ()=>{},
+        ()=>{}
+      )
+    }, [])
+  return (
+    <div>
+      { user && <ShipItems />}
+    </div>
+  )
+}
