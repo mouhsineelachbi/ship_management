6ad6dccf12d2f17adcbd665f62ed2ecfd012f20c
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-04-23 22:02:37 +0000

    navigate to login when user not logged in

--- client_app/src/components/home.component/home.component.jsx
+++ client_app/src/components/home.component/home.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from 'react-router-dom';
 import {getUser} from "../../feature/user/user.slice";
 import ShipItems from '../ship.items.component/ship.items.component';
 
@@ -8,13 +9,15 @@ export default function Home() {
 
   const dispatch = useDispatch();
   const {userLoading, user, error} = useSelector(state => state.user);
+  const navigate = useNavigate()
 
     useEffect(()=>{
       dispatch(getUser())
+      .unwrap()
       .then(
         ()=>{},
         ()=>{
-          
+          navigate("/login");
         }
       )
     }, [])
